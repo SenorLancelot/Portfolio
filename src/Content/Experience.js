2d1b5f2199fa9ae2703a9fa31e5502003874357b
Amarendra Shendkar <amarshendkar.as@gmail.com>
2023-08-11 21:28:13 +0530

    updates experience

--- src/Content/Experience.js
+++ src/Content/Experience.js
@@ -7,6 +7,7 @@ export const ExperienceContent = [
       "Developed a Feed Generator in Java Springboot along with utility classes and functions for data retrieval,filtering, and congregation into excel to generate a report based on given filter criteria and reduce manual effort by 90%",
       "Composed UNIX scripts and accelerated the data warehousing pipeline by automating the processes feed generations, packaging them with respective check-sums and .info files and publishing them to a central data warehouse.",
       "Mastered multiple tools in the Java application development and deployment pipeline, including package management using Maven, Testing using Jconsole, Versioning using Git and building using Jenkins.",
+      "Developed a ReactJS frontend for an Automation as a Service (AaaS) application developed on Java to handle automated testing using the RobotFramework, reducing manual effort and redundancy.",
       "Created Natural Language Processing (SBERT) pipeline in Python along with MS excel to find similarity indexes through multiple sentence encoding algorithms and aggregate similar responses to business life cycle events to reduce duplication in controls across different regions and business units by 60%.",
     ],
   },
@@ -18,6 +19,9 @@ export const ExperienceContent = [
       "Collaborated as a Full Stack Web Developer to design an Enterprise Resource Planning System with databases and implemented API(s) for multiple integrated modules such as Core (Departments, Designations, Employees management), Attendances, Leaves using PostgreSQL Django Rest Framework.",
       "Designed a custom renderer and parser for JSON objects for much improved data visualization and handling.",
       "Introduced MPTT to incorporate hierarchical models. Designed a custom backend filter for 30% efficient selective retrieval of data. Built Celery tasks for automation of tasks like leave generation and leave handling.",
+      "Dockerized entire project to enable cross platform deployment and consistency.",
+      "Generated Postman and Swagger API specs for easy testing and handling.",
+      "Documented entire module in overleaf using LaTeX for trouble free handover.",
     ],
   },
 ];
